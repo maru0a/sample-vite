65af260eb684f9c6838b3d642823936dadaf7f14
maru0a <yao452191@gmail.com>
2025-03-16 22:53:45 +0900

    add: MVP2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,60 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const records = [
-    { title: "勉強の記録1", time: 1 },
-    { title: "勉強の記録2", time: 3 },
-    { title: "勉強の記録3", time: 5 },
-  ];
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState(0);
+  const [records, setRecords] = useState([]);
+  const [error, setError] = useState("");
+
+  const addRecord = () => {
+    setError("");
+    //空の場合は登録しない
+    if (title === "" || time === "") return setError("入力してください");
+
+    setRecords([
+      ...records,
+      {
+        title: title,
+        time: time,
+      },
+    ]);
+
+    setTitle("");
+    setTime(0);
+  };
 
   return (
     <>
       <h1>学習記録一覧</h1>
+      <div>
+        <div>
+          学習内容：
+          <input
+            type="text"
+            value={title}
+            placeholder="学習内容を記入してください"
+            onChange={(event) => setTitle(event.target.value)} //MEMO：event.target.valueで詰まった
+          />
+        </div>
+        <div>
+          学習時間：
+          <input
+            type="number"
+            value={time}
+            placeholder="学習時間を記入してください"
+            onChange={(event) => setTime(event.target.value)}
+          />
+        </div>
+        <div>{error}</div>
+        <button onClick={() => addRecord()}>追加</button>
+        {/* ??：addRecordだとダメだっけ、addRecord()なら動いたけど変数指定していない時もこれだっけという */}
+      </div>
+      <div>
+        入力中：
+        <div>学習内容：{title}</div>
+        <div>学習時間：{time}時間</div>
+      </div>
       <ul>
         {records.map((item) => {
           return (
